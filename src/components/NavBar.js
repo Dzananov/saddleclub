7feb025046dbcb3c95189c696b5f86b2819a7eb9
Dzananov <danni_daa@hotmail.com>
2022-11-08 12:15:11 +0000

    basic style add to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.jpg";
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = () => {
     return (
-      <Navbar expand="md" fixed="top">
+      <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
           <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
@@ -13,7 +14,7 @@ const NavBar = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
               <Nav.Link>
-                <i className="fas fa-home"></i>Home
+                <i className="fas fa-home-heart"></i>Home
               </Nav.Link>
               <Nav.Link>
                 <i className="fas fa-sign-in-alt"></i>Sign in
